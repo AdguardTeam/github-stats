2942c7f7cd71e10dc1a45bb7d83bbb74b9bf4236
Stanislav A <s.atroschenko@adguard.com>
2022-04-27 13:14:26 +0300

    change txt > json

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 const EVENT_EXPIRATION_DAYS = 30;
-const STORAGE_PATH = './events-collection.txt';
+const STORAGE_PATH = './events-collection.json';
 const ENDPOINTS = {
     GITHUB_EVENTS: 'GET /repos/{owner}/{repo}/events',
 };
