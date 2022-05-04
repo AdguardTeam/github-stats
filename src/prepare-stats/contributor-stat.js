7e0d9f33236b25f5ac733c94b5e9cc6e790daa58
Stanislav A <s.atroschenko@adguard.com>
2022-05-04 13:16:25 +0300

    improve prepareContributorStat func

--- src/prepare-stats/contributor-stat.js
+++ src/prepare-stats/contributor-stat.js
@@ -1,64 +1,24 @@
 const Contributor = require('./Contributor');
 const {
-    isStale,
+    getActivityAuthor,
 } = require('../tools/events-utils');
-const {
-    EVENT_TYPES,
-} = require('../constants');
 
 const prepareContributorStat = (events) => {
-    const contributors = {};
-
-    /**
-     * Checks if event counts as activity and returns corresponding contributor
-     * @param {Object} events array with GitHub event objects
-     * @return {string|undefined} returns author name for activities and undefined for other events
-     */
-    const getActivityAuthor = (event) => {
-        const { type, payload, actor } = event;
-        const username = actor.login;
-        let contributorName;
-        switch (type) {
-            case EVENT_TYPES.PULL_REQUEST_REVIEW_EVENT:
-            case EVENT_TYPES.ISSUE_COMMENT_EVENT:
-            case EVENT_TYPES.PUSH_EVENT: {
-                contributorName = username;
-                break;
-            }
-            case EVENT_TYPES.ISSUES_EVENT: {
-                if (payload.action === 'closed' && !isStale(payload.issue)) {
-                    contributorName = username;
-                }
-                break;
-            }
-            case EVENT_TYPES.PULL_REQUEST_EVENT: {
-                // count only newly opened & merged pulls
-                if (payload.action === 'opened'
-                    || (payload.action === 'closed' && typeof payload.pull_request.merged_at === 'string')) {
-                    // merged pull request count for the one who opened it
-                    contributorName = payload.pull_request.user.login;
-                }
-                break;
-            }
-            default:
-                break;
-        }
-
-        return contributorName;
-    };
-
-    events.forEach((event) => {
+    const contributors = events.reduce((acc, event) => {
         const contributorName = getActivityAuthor(event);
         // Events that don't count as activity are not saved anywhere
         if (!contributorName) {
-            return;
+            return acc;
         }
         // Init contributor if there isn't one
-        if (!contributors[contributorName]) {
-            contributors[contributorName] = new Contributor();
+        if (!acc[contributorName]) {
+            acc[contributorName] = new Contributor();
         }
-        contributors[contributorName].addActivityEvent(event);
-    });
+        acc[contributorName].addActivityEvent(event);
+
+        return acc;
+    }, {});
+
     return contributors;
 };
 
--- src/tools/events-utils.js
+++ src/tools/events-utils.js
@@ -161,6 +161,44 @@ const sortEventsByHour = (contributor) => {
     return activityByHour;
 };
 
+/**
+ * Checks if event counts as activity and returns corresponding contributor
+ * @param {Object} events array with GitHub event objects
+ * @return {string|undefined} returns author name for activities and undefined for other events
+ */
+const getActivityAuthor = (event) => {
+    const { type, payload, actor } = event;
+    const username = actor.login;
+    let contributorName;
+    switch (type) {
+        case EVENT_TYPES.PULL_REQUEST_REVIEW_EVENT:
+        case EVENT_TYPES.ISSUE_COMMENT_EVENT:
+        case EVENT_TYPES.PUSH_EVENT: {
+            contributorName = username;
+            break;
+        }
+        case EVENT_TYPES.ISSUES_EVENT: {
+            if (payload.action === 'closed' && !isStale(payload.issue)) {
+                contributorName = username;
+            }
+            break;
+        }
+        case EVENT_TYPES.PULL_REQUEST_EVENT: {
+            // count only newly opened & merged pulls
+            if (payload.action === 'opened'
+                || (payload.action === 'closed' && typeof payload.pull_request.merged_at === 'string')) {
+                // merged pull request count for the one who opened it
+                contributorName = payload.pull_request.user.login;
+            }
+            break;
+        }
+        default:
+            break;
+    }
+
+    return contributorName;
+};
+
 module.exports = {
     removeOldEvents,
     isOpenedAction,
@@ -171,4 +209,5 @@ module.exports = {
     getCommitsCount,
     countEventsByType,
     sortEventsByHour,
+    getActivityAuthor,
 };
