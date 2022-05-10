4bc8efa537285ad19992614d8295e525fb2514f3
Stanislav A <s.atroschenko@adguard.com>
2022-05-10 21:19:35 +0300

    explicitly check access to collection file and create one if needed

--- src/poll-events.js
+++ src/poll-events.js
@@ -1,11 +1,16 @@
 const { unionBy } = require('lodash/array');
 const { removeOldEvents } = require('./tools/events-utils');
-const { getEventsFromCollection, writeEventsToCollection } = require('./tools/fs-utils');
 const { getGithubEvents } = require('./tools/gh-utils');
 const { EVENT_EXPIRATION_DAYS } = require('./constants');
+const {
+    getEventsFromCollection,
+    writeEventsToCollection,
+    createFileAccess,
+} = require('./tools/fs-utils');
 
 const pollEvents = async (collectionPath, requestData) => {
     const newEvents = await getGithubEvents(requestData);
+    await createFileAccess(collectionPath);
     let collection = await getEventsFromCollection(collectionPath);
     collection = removeOldEvents(collection, EVENT_EXPIRATION_DAYS);
     // Merge polled events with storage and de-duplicate by id
--- src/tools/fs-utils.js
+++ src/tools/fs-utils.js
@@ -1,4 +1,10 @@
-const fs = require('fs').promises;
+const {
+    appendFile,
+    readFile,
+    writeFile,
+    access,
+} = require('fs').promises;
+const { constants } = require('fs');
 
 /**
  * Gets array of GitHub event objects from file
@@ -6,10 +12,7 @@ const fs = require('fs').promises;
  * @return {Array.<Object>} array with GitHub event objects
  */
 const getEventsFromCollection = async (path) => {
-    const file = await fs.readFile(path, {
-        encoding: 'utf8',
-        flag: 'as+',
-    });
+    const file = await readFile(path, 'utf8');
     if (file.length === 0) {
         return [];
     }
@@ -23,10 +26,28 @@ const getEventsFromCollection = async (path) => {
  * @param {Array.<Object>} events array with GitHub event objects
  */
 const writeEventsToCollection = async (path, events) => {
-    await fs.writeFile(path, JSON.stringify(events));
+    await writeFile(path, JSON.stringify(events));
+};
+
+/**
+ * Create file on a given path if there is none
+ * @param {string} path path to the file
+ * @return {boolean}
+ */
+const createFileAccess = async (path) => {
+    try {
+        /* eslint-disable no-bitwise */
+        await access(path, constants.F_OK
+            | constants.R_OK
+            | constants.W_OK);
+        /* eslint-enable no-bitwise */
+    } catch {
+        await appendFile(path, '', 'utf8');
+    }
 };
 
 module.exports = {
     getEventsFromCollection,
     writeEventsToCollection,
+    createFileAccess,
 };
